47d935710d961bce9a46eb4c1f41810957eab538
Noe Game <noe.game91@gmail.com>
2025-12-01 21:34:54 +0100

    id -> aruco_id

--- raspberry/src/my_math.py
+++ raspberry/src/my_math.py
@@ -16,7 +16,7 @@ class Point:
         self.aruco_id = aruco_id
 
     def __str__(self):
-        return f"Point(x={self.x}, y={self.y}, ID={self.ID}, angle={self.angle})"
+        return f"Point(x={self.x}, y={self.y}, aruco_id={self.aruco_id}, angle={self.angle})"
     
     def __print__(self):
         return self.__str__()
@@ -27,21 +27,21 @@ def distance(p1: Point, p2: Point) -> float:
 
 def find_point_by_id(points: list[Point], target_id: int):
     """
-    Trouve un point par son ID dans une liste de points.
+    Trouve un point par son aruco_id dans une liste de points.
 
     Args:
         points: La liste des points dans laquelle chercher.
-        target_id: L'ID du point à trouver.
+        target_id: L'aruco_id du point à trouver.
 
     Returns:
-        Le Point correspondant à l'ID, ou None si aucun point n'est trouvé.
+        Le Point correspondant à l'aruco_id, ou None si aucun point n'est trouvé.
     """
     for p in points:
-        if p.ID == target_id:
+        if p.aruco_id == target_id:
             return p
     return None
 
 def print_points(points : list) -> None:
     """Affiche une liste de points"""
     for i in range(len(points)):
-        print(f"{i}: {points[i].ID} -> ({points[i].x}, {points[i].y})")
+        print(f"{i}: {points[i].aruco_id} -> ({points[i].x}, {points[i].y})")
